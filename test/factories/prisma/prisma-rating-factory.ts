1b5ece1832aa31f2aad9769678d9ef7bcf4a1bbd
João Cortabitart <joaovictorcortabitart@gmail.com>
2026-02-14 13:02:23 -0300

    fix: 🐛 fixing tests

---
+++ test/factories/prisma/prisma-rating-factory.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import {
+  Rating,
+  RatingProps,
+} from 'src/domain/ecommerce/enterprise/entities/rating';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+import { makeRating } from '../make-rating';
+import { PrismaRatingMapper } from 'src/infra/database/prisma/mappers/prisma-rating-mapper';
+
+@Injectable()
+export class RatingFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaRating(data: Partial<RatingProps> = {}): Promise<Rating> {
+    const rating = makeRating(data);
+
+    await this.prisma.rating.create({
+      data: PrismaRatingMapper.toPrisma(rating),
+    });
+
+    return rating;
+  }
+}
